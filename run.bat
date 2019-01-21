880c8dafdda1502f307f5b5778a795f673e071de
Aboud <aboud.zakaria@gmail.com>
2019-01-21 17:07:21 +0100

    increase java heap memory

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 REM batch file to run frodo
-java.exe -jar "frodo2.16\\frodo.jar" "test.xml"
+java.exe -Xms1024m -Xmx2048m -jar "frodo2.16\\frodo2.jar" "test.xml"
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar frodo2.16/frodo2.jar test.xml
+java -Xms1024m -Xmx2048m -jar frodo2.16/frodo2.jar test.xml
 
