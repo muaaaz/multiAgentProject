1f7a506e51ce643703b5e2a937b5084d75a61a1d
Aboud <aboud.zakaria@gmail.com>
2019-01-21 20:17:47 +0100

    parameter output

--- console.sh
+++ console.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms1024m -Xmx20480m -cp frodo2.16/frodo2.jar frodo2.controller.Controller -local | tee output.log
+java -Xms1024m -Xmx20480m -cp frodo2.16/frodo2.jar frodo2.controller.Controller -local | tee $1
